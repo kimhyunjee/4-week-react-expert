1777788e1340929498c2994a6aacdefd641984c6
내꺼다\ysk30 <ysk30430@naver.com>
2022-08-04 00:31:29 +0900

    header추가

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+    return (
+
+        <Container>
+        <div>MT Todo List</div>
+        <div>React-2-week</div>
+        </Container>
+
+    );
+};
+
+const Container = styled.header`
+    align-items: center;
+    border: 1px solid #ddd;
+    display: flex;
+    height: 50px;
+    justify-content: space-between;
+    padding: 0 20px;
+    background-color: #d7d1e9;
+`
+
+export default Header;
