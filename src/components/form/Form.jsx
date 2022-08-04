39dd9ad21cd7fb9a3daebb10b95fba142f21068c
내꺼다\ysk30 <ysk30430@naver.com>
2022-08-05 01:42:02 +0900

    Form컴포넌트 CSS정리

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -38,17 +38,12 @@ function Form() {
         return (
             <FormContainer >
                 <InputGroup>
-                <div>
-                <label>제목</label>
-                <Input type="text" ref={title}></Input>
-                <label>내용</label>
-                <Input type="text" ref={comment}></Input>
-                </div>
-                <div>
-                <Button onClick={addButton} > 추가하기 </Button>
-                </div>
+                    <label>제목</label>
+                    <input type="text" ref={title} />
+                    <label>내용</label>
+                    <input type="text" ref={comment} />
                 </InputGroup>
-    
+                    <Button onClick={addButton} > 추가하기 </Button>
             </FormContainer>
         );
 };
@@ -59,27 +54,35 @@ function Form() {
 // & > 자기자신 바로 아래에 있는 태그에만 적용
 
 const FormContainer = styled.form`
+    display: flex;
+    align-items: center;
     background-color: #f9f6e3;
     border-radius: 12px;
-    /* justify-content: space-between; */
+    justify-content: space-between;
     margin: 0 auto;
     padding: 30px;
-`
+`;
 const InputGroup = styled.div`
-    justify-content: space-between;
+    /* justify-content: space-between; */
     align-items: center;
     display: flex;
-    gap: 20px;
-`
-
-const Input = styled.input`
-
+    gap: 10px;
+& input {
     border: none;
     border-radius: 12px;
     height: 40px;
     padding: 0 12px;
     width: 240px;
-`
+}
+`;
+
+// const Input = styled.input`
+//     border: none;
+//     border-radius: 12px;
+//     height: 40px;
+//     padding: 0 12px;
+//     width: 240px;
+// `;
 
 const Button = styled.button`
     background-color: #8455c5;
@@ -89,7 +92,7 @@ const Button = styled.button`
     font-weight: 700;
     height: 40px;
     width: 140px;
-`
+`;
 
 
 export default Form;
