d084bb4dccaf33dde7dda07033af3f0863861d17
내꺼다\ysk30 <ysk30430@naver.com>
2022-08-05 02:59:40 +0900

    삭제버튼, 완료버튼 액션생성함수 완성, 리듀서 완성

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,30 +1,28 @@
 //redux.modules.todos.js
 
-
-import { useDispatch } from "react-redux";
-
 // Actions type을 정해주는곳
-const ADD = "todo/ADD";
-// const DELETE = "TODO/DELETE";
+const ADDTODO = "todo/ADDTODO";
+const DELETETODO = "todo/DELETETODO";
 // const LOAD = "todo/LOAD";
-// const TOGGLE = "todo/TOGGLE";
+const TOGGLETODO = "todo/TOGGLETODO";
 
 
 //initial state 초기값
-const initialState = {todos : [
+const initialState = {
+    todos : [
     { 
     id:1, 
-    titleText:"리액트 공부하기" , 
-    commentText:"리액트 어렵다" , 
-    inDone: false, 
+    title:"리액트 공부하기" , 
+    comment:"리액트 어렵다" , 
+    isDone: false, 
     },
     {
     id:2,
-    titleText: "리액트에서 중요한것",
-    commentText: "값을 가져오는 것과 함수명/변수명 잘짓기",
-    isDone: false,
+    title: "리액트에서 중요한것",
+    comment: "값을 가져오는 것과 함수명/변수명 잘짓기",
+    isDone: true,
     },
-],todo:{}
+],
 };
 
 
@@ -32,33 +30,43 @@ const initialState = {todos : [
 
 // Action Creators 액션타입에 대한 함수 생성
 export const addTodo = (todo) => {
-    return {type:ADD, todo };
+    // console.log(todo);
+    return { type:ADDTODO, todo };
 }
-// export const deleteTodo (todo_id) => {
-//     return {type: DELETE, todo_di};
-// }
+export const deleteTodo = (todo_id) => {
+    return {type: DELETETODO, todo_id};
+}
 // export const LOAD (todo_list) => {
 //     return {type: LOAD, todo_list};
 // }
-// export const toggleTodo (todo_id) => {
-//     return {type: DELETE, todo_di};
-// }
+export const toggleTodo = (todo_id) => {
+    return {type: TOGGLETODO, todo_id};
+}
 
 
 // Reducer 모듈파일에서는 리듀서를 export default한다
 export default function reducer(state = initialState, action ) {
     //파라미터 = 이거는 state에 아무 값이 오지 않아도 기본값을 가지고있어란뜻
     switch(action.type){
-        case "todo/ADD" : {
+        case "todo/ADDTODO" : {
             const new_todo = [...state.todos, action.todo];
             return {...state, todos:new_todo};
         }
-        // case "todo/DELETE":
-        // const new_todo = state.todos.filter((todo) => {
-        //     return action.todo_id !== todo.id;
-        // });
-        // return {...state,todos:new_todo};
-        //return {list:state.list.filter(todo => todo.id !== action.deleteId)}
+        case "todo/DELETETODO": {
+        const new_todo = state.todos.filter((todo) => {
+            return action.todo_id !== todo.id;
+        });
+        return {...state,todos:new_todo};
+        }
+        case "todo/TOGGLETODO" : {
+            const new_todo = state.todos.map((todo) => {
+                if (action.todo_id === todo.id) {
+                    todo.isDone = !todo.isDone
+                }
+                return todo
+            });
+            return {...state, todos:new_todo};
+            }
 
         default: return state;
     };
